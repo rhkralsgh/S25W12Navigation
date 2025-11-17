83e48d131346133a370ffe0cf228319e45589df2
rhkralsgh <knh239@naver.com>
2025-11-17 16:38:37 +0900

    Initial Commit

---
+++ S25W12Navigation/S25W12NavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  S25W12NavigationApp.swift
+//  S25W12Navigation
+//
+//  Created by student on 11/17/25.
+//
+
+import SwiftUI
+
+@main
+struct S25W12NavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
