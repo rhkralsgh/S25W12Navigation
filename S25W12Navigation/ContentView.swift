8576193dd84c7a026270dca225a2447d781d667b
rhkralsgh <knh239@naver.com>
2025-11-19 14:38:12 +0900

    ✨Detail View

--- S25W12Navigation/ContentView.swift
+++ S25W12Navigation/ContentView.swift
@@ -1,26 +1,52 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var viewModel = SongViewModel()
+    
     var body: some View {
         NavigationStack {
-            Text("노래 화면")
-
-            NavigationLink(destination: SingerView()) {
-                Text("가수 화면으로 이동")
+            List(viewModel.songs) { song in
+                NavigationLink(destination: SongDetailView(song: song)) {
+                    HStack {
+                        VStack(alignment: .leading) {
+                            Text(song.title)
+                                .font(.headline)
+                            Text(song.singer)
+                                .font(.subheadline)
+                                .foregroundColor(.gray)
+                        }
+                    }
+                }
             }
         }
     }
 }
 
-struct SingerView: View {
+struct SongDetailView: View{
+    let song: Song
+    
     var body: some View {
-        NavigationStack {
-            Text("가수 화면")
-
-//            NavigationLink(destination: ContentView()) {
-//                Text("노래 화면으로 이동")
-//            }
+        ScrollView {
+            VStack(alignment: .leading, spacing: 15) {
+                HStack {
+                    Text(song.singer)
+                        .font(.title2)
+                        .foregroundColor(.secondary)
+                    Spacer()
+                    Text(String(song.rating))
+                        .font(.title)
+                        .foregroundColor(.yellow)
+                }
+                .padding(.bottom, 10)
+                
+                Text(song.lyrics)
+                    .font(.body)
+                    .multilineTextAlignment(.leading)
+            }
+            .padding()
         }
+        .navigationTitle(song.title)
+        .navigationBarTitleDisplayMode(.large)
     }
 }
 
